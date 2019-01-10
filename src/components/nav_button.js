930425b292e0bce264c2c2f9239d2250d84c9b52
Kur0a5h <kuroash.esmaili@gmail.com>
2019-01-10 15:46:48 -0800

    Completed add item functionality

--- src/components/nav_button.js
+++ src/components/nav_button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 export default props => (
-    <div classname='row'>
+    <div className='row'>
         <div className="col s12 right-align">
             <Link 
             className={`btn ${props.color || 'light-green accent-3'}`}
